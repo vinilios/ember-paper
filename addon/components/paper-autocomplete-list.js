496925699fda036ab78f01a61a614bd0d2f610d9
Kostas Papadimitriou <kpap@grnet.gr>
2016-07-26 18:30:22 +0300

    Update autocomplete and virtual-repeat components

--- addon/components/paper-autocomplete-list.js
+++ addon/components/paper-autocomplete-list.js
@@ -8,6 +8,22 @@ const { Component } = Ember;
 const ITEM_HEIGHT = 41;
 const MAX_HEIGHT = 5.5 * ITEM_HEIGHT;
 const MENU_PADDING = 8;
+const INPUT_PADDING = 2;
+
+/**
+ * Calculate the positive scroll offset
+ * TODO: Check with pinch-zoom in IE/Chrome;
+ *       https://code.google.com/p/chromium/issues/detail?id=496285
+ */
+const scrollTop = function(element) {
+  element = $(element || document.body);
+
+  var body = (element[0] == document.body) ? document.body : undefined;
+  var scrollTop = body ? body.scrollTop + body.parentElement.scrollTop : 0;
+
+  // Calculate the positive scroll offset
+  return scrollTop || Math.abs(element[0].getBoundingClientRect().top);
+};
 
 /**
  * @class PaperAutocompleteList
@@ -45,70 +61,94 @@ export default Component.extend({
 
   // TODO reafactor into a computed property that binds directly to dropdown's `style`
   positionDropdown() {
-    let wrapper = Ember.$(`#${this.get('wrapToElementId')}`)[0];
-    if (wrapper.firstElementChild.tagName.toLowerCase() == "md-input-container") {
-      wrapper = Ember.$(`#${this.get('wrapToElementId')}`).find("input")[0];
+    let floating = this.get('floating');
+    let wrap = Ember.$(`#${this.get('wrapToElementId')}`)[0];
+    let snap = wrap;
+    let _root = document.body;
+    let element = this.$();
+    let scrollContainer = element.parent().parent();
+
+
+    //TODO: if !elements schedule positionDropdown for the next render cycle
+
+    var hrect  = wrap.getBoundingClientRect(),
+        vrect  = snap.getBoundingClientRect(),
+        root   = _root.getBoundingClientRect(),
+        top    = vrect.bottom - root.top,
+        bot    = root.bottom - vrect.top,
+        left   = hrect.left - root.left,
+        width  = hrect.width,
+        offset = getVerticalOffset(),
+        styles;
+
+    // Adjust the width to account for the padding provided by `md-input-container`
+    if (floating) {
+      left += INPUT_PADDING;
+      width -= INPUT_PADDING * 2;
     }
-    let hrect  = wrapper.getBoundingClientRect();
-    let vrect  = hrect;
-    let root   = document.body.getBoundingClientRect();
-    let top    = vrect.bottom - root.top;
-    let bot    = root.bottom - vrect.top;
-    let left   = hrect.left - root.left;
-    let { width }  = hrect;
-    let styles = {
-        left: `${left}px`,
-        minWidth: `${width}px`,
-        maxWidth: `${Math.max(hrect.right - root.left, root.right - hrect.left) - MENU_PADDING}px`
-      };
-
-    let ul = this.$();
-    let sizer = ul.parent().find(".md-virtual-repeat-sizer");
-    let cont = ul.parent().parent();
-    let maxHeight;
-
+    styles = {
+      left:     left + 'px',
+      minWidth: width + 'px',
+      maxWidth: Math.max(hrect.right - root.left, root.right - hrect.left) - MENU_PADDING + 'px'
+    };
     if (top > bot && root.height - hrect.bottom - MENU_PADDING < MAX_HEIGHT) {
-      styles.top = 'auto';
-      styles.bottom = `${bot}px`;
-      maxHeight = Math.min(MAX_HEIGHT, hrect.top - root.top - MENU_PADDING);
+      styles.top       = 'auto';
+      styles.bottom    = bot + 'px';
+      styles.maxHeight = Math.min(MAX_HEIGHT, hrect.top - root.top - MENU_PADDING) + 'px';
     } else {
-      styles.top = `${top}px`;
-      styles.bottom = 'auto';
-      maxHeight = Math.min(MAX_HEIGHT, root.bottom - hrect.bottom - MENU_PADDING);
+      styles.top       = (top - offset) + 'px';
+      styles.bottom    = 'auto';
+      styles.maxHeight = Math.min(MAX_HEIGHT, root.bottom + scrollTop() - hrect.bottom - MENU_PADDING) + 'px';
     }
-    styles.maxHeight = `${maxHeight}px`;
-    cont.css(styles);
-    correctHorizontalAlignment();
-    
+
+    scrollContainer.css(styles);
     Ember.run.scheduleOnce('render', this, function() {
-      if (sizer.height() < maxHeight) {
-       cont.css({height: sizer.height()});
-      } else {
-       cont.css({height: maxHeight})
-      }
+      correctHorizontalAlignment();
     });
 
-    /*
+    /**
+     * Calculates the vertical offset for floating label examples to account for messages
+     * @returns {number}
+     */
+    function getVerticalOffset () {
+      var offset = 0;
+      var inputContainer = $(wrap).find('md-input-container');
+      if (inputContainer.length) {
+        var input = inputContainer.find('input');
+        offset = inputContainer.prop('offsetHeight');
+        offset -= input.prop('offsetTop');
+        offset -= input.prop('offsetHeight');
+        // add in the height left up top for the floating label text
+        offset += inputContainer.prop('offsetTop');
+      }
+      return offset;
+    }
+
+    /**
      * Makes sure that the menu doesn't go off of the screen on either side.
      */
-    function correctHorizontalAlignment() {
-      let dropdown = cont[0].getBoundingClientRect();
-      let styles = {};
+    function correctHorizontalAlignment () {
+      var dropdown = scrollContainer[0].getBoundingClientRect(),
+          styles   = {};
       if (dropdown.right > root.right - MENU_PADDING) {
-        styles.left = `${hrect.right - dropdown.width}px`;
+        styles.left = (hrect.right - dropdown.width) + 'px';
       }
-      cont.css(styles);
+      scrollContainer.css(styles);
     }
   },
 
-  observeIndex: Ember.observer('selectedIndex', function() {
+  updateScroll() {
     let suggestions = this.get('suggestions');
-    if (!suggestions || !suggestions.objectAt(this.get('selectedIndex'))) {
+    if (!suggestions || 
+        !suggestions.objectAt(this.get('selectedIndex'))) {
       return;
     }
 
     let ul = this.$();
     let li  = ul.find(`li:eq(${this.get('selectedIndex')})`).get(0);
+    if (li === undefined) {
+      Ember.run.scheduleOnce('render', this, 'updateScroll');
+    }
     let top = li.offsetTop;
     let bot = top + li.offsetHeight;
     let hgt = ul[0].clientHeight;
@@ -118,7 +158,7 @@ export default Component.extend({
     } else if (bot > ul[0].scrollTop + hgt) {
       ul[0].scrollTop = bot - hgt;
     }
-  }),
+  },
 
   resizeWindowEvent() {
     this.positionDropdown();
--- addon/components/paper-virtual-repeat.js
+++ addon/components/paper-virtual-repeat.js
@@ -6,7 +6,7 @@ import ColorMixin from 'ember-paper/mixins/color-mixin';
 import FlexMixin from 'ember-paper/mixins/flex-mixin';
 
 const decodeEachKey = Ember.__loader.require('ember-htmlbars/utils/decode-each-key')['default'];
-const {set, get, observer} = Ember;
+const {set, get, observer, computed} = Ember;
 
 class Cell {
   constructor(key, item, index, style) {
@@ -28,6 +28,7 @@ export default EmberCollection.extend(ColorMixin, FlexMixin, {
   
   init() {
     this._super();
+    this.cellItems = Ember.A();
     if (!this.getAttr('cell-layout')) {
       // single column layout by default
       this.attrs['cell-layout'] = new Grid(100000000000000000, 48);
@@ -37,132 +38,21 @@ export default EmberCollection.extend(ColorMixin, FlexMixin, {
     }
   },
 
-  // we override update cells to patch 
-  // https://github.com/emberjs/ember-collection/pull/98
-  nativeUpdateCells() {
-    if (!this._items) { return; }
-    const numItems = get(this._items, 'length');
-    if (this._cellLayout.length !== numItems) {
-      this._cellLayout.length = numItems;
-    }
-
-    var priorMap = this._cellMap;
-    var cellMap = Object.create(null);
-
-    var index = this._cellLayout.indexAt(this._scrollLeft, this._scrollTop, this._clientWidth, this._clientHeight);
-    var count = this._cellLayout.count(this._scrollLeft, this._scrollTop, this._clientWidth, this._clientHeight);
-    var items = this._items;
-    var bufferBefore = Math.min(index, this._buffer);
-    index -= bufferBefore;
-    count += bufferBefore;
-    count = Math.min(count + this._buffer, get(items, 'length') - index);
-    var i, style, itemIndex, itemKey, cell;
-
-    var newItems = [];
-
-    for (i=0; i < count; i++) {
-      itemIndex = index + i;
-      itemKey = decodeEachKey(items.objectAt(itemIndex), '@identity');
-      if (priorMap) {
-        cell = priorMap[itemKey];
-      }
-      if (cell) {
-        style = this._cellLayout.formatItemStyle(itemIndex, this._clientWidth, this._clientHeight);
-        set(cell, 'style', style);
-        set(cell, 'hidden', false);
-        set(cell, 'key', itemKey);
-        cellMap[itemKey] = cell;
-      } else {
-        newItems.push(itemIndex);
-      }
-    }
-
-    for (i=0; i<this._cells.length; i++) {
-      cell = this._cells[i];
-      if (!cellMap[cell.key]) {
-        if (newItems.length) {
-          itemIndex = newItems.pop();
-          let item = items.objectAt(itemIndex);
-          itemKey = decodeEachKey(item, '@identity');
-          style = this._cellLayout.formatItemStyle(itemIndex, this._clientWidth, this._clientHeight);
-          set(cell, 'style', style);
-          set(cell, 'key', itemKey);
-          set(cell, 'index', itemIndex);
-          set(cell, 'item', item);
-          set(cell, 'hidden', false);
-          cellMap[itemKey] = cell;
-        } else {
-          set(cell, 'hidden', true);
-          set(cell, 'style', 'height: 0; display: none;');
-        }
-      }
-    }
-
-    for (i=0; i<newItems.length; i++) {
-      itemIndex = newItems[i];
-      let item = items.objectAt(itemIndex);
-      itemKey = decodeEachKey(item, '@identity');
-      style = this._cellLayout.formatItemStyle(itemIndex, this._clientWidth, this._clientHeight);
-      cell = new Cell(itemKey, item, itemIndex, style);
-      cellMap[itemKey] = cell;
-    }
-
-    // hack to keep things in the correct order
-    var cells = [];
-    for (i=0; i < count; i++) {
-      itemIndex = index + i;
-      itemKey = decodeEachKey(items.objectAt(itemIndex), '@identity');
-      cells.push(cellMap[itemKey]);
-    }
-    set(this, '_cells', cells);
-    this._cellMap = cellMap;
+  _updateItems: function() {
+    let items = this.get('items');
+    let cells = this.get('_cells');
+    this.get('cellItems').setObjects(
+      this.get('_cells').filter((i) => !i.hidden).map((i) => items[i.index]));
+      console.log("update items");
+    this.observeScrollTop();
   },
 
-  updateCells() {
-    this.nativeUpdateCells();
-
-    let layout = this.getAttr('cell-layout');
-    let topIndex = get(this, 'topIndex');
-    if (topIndex >= 0 && topIndex < layout.length && !get(this, '_topIndexSet')) {
-      let offset = layout.positionAt(topIndex);
-      set(this, '_scrollLeft', offset.x);
-      set(this, '_scrollTop', offset.y);
-      set(this, '_topIndexSet', true);
-      this._needsRevalidate();
-    }
-
-    if (!get(this, '_elementSizeSet')) {
-      let el = this.$();
-      let firstCell = this._cells && this._cells[0];
-      if (el && el.length && firstCell) {
-        layout.bin._elementWidth = el[0].clientWidth;
-
-        // skip current render cycle
-        Ember.run.scheduleOnce('render', this, function() {
-          let view = el.find('[auto-size]').first();
-          if (view && view.length && document.body.contains(view[0])) {
-            layout.bin._elementHeight = view[0].offsetHeight;
-            set(this, '_elementSizeSet', true);
-            this._needsRevalidate();
-          }
-        });
-      }
-
-      // avoid endless auto-size discovery
-      let resizeLimit = 10;
-      set(this, '_elementSizeRetry', get(this, '_elementSizeRetry') + 1);
-      if (get(this, '_elementSizeRetry') > resizeLimit) {
-        set(this, '_elementSizeSet', true);
-      }
-    }
+  updateCells: function() {
+    this._super();
+    this._updateItems();
   },
 
-  observeTopIndex: observer('topIndex', function() {
-    set(this, '_topIndexSet', false);
-    this.updateCells();
-  }),
-
-  observeScrollTop: observer('_scrollTop', '_contentSize.height', function() {
+  observeScrollTop: function() {
     if (!this.element) { return; }
     this.listElement = this.element.firstElementChild.children[1];
     if (!this.listElement) {
@@ -170,21 +60,21 @@ export default EmberCollection.extend(ColorMixin, FlexMixin, {
     }
 
     let topTranslate;
-    let items = get(this, 'items.length');
+    let items = this._items;
     let offset = get(this, '_scrollTop');
     let itemSize = get(this, '_cellLayout.bin._elementHeight');
-    let numExtra = get(this, '_buffer') || 0;
-    let numItems = Math.max(0, Math.floor(offset / itemSize) - numExtra);
+    let index = this._cellLayout.indexAt(this._scrollLeft, this._scrollTop, this._clientWidth, this._clientHeight);
+    let count = this._cellLayout.count(this._scrollLeft, this._scrollTop, this._clientWidth, this._clientHeight);
+    let bufferBefore = Math.min(index, this._buffer);
 
-    if (numItems > items) {
-      numItems = items;
-    }
-    topTranslate = numItems * itemSize;
+    index -= bufferBefore;
+    topTranslate = (index * itemSize); // TODO: fixme
+    console.log(index, count);
     this.listElement.style.position = 'absolute';
     this.listElement.style.top = 0;
     this.listElement.style.bottom = 0;
     this.listElement.style.left = 0;
     this.listElement.style.right = 0;
     this.listElement.style.transform = `translateY(${topTranslate}px)`;
-  })
+  }
 });
